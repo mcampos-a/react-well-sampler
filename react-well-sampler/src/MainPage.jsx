9e167182d64897f6076a0013dae34ccce84c360c
Max Campos <mcampos2a@gmail.com>
2025-06-28 15:45:01 +0200

    create MainPage component for well sampler landing page

---
+++ react-well-sampler/src/MainPage.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+
+const MainPage = () => {
+
+    return (
+        <div>
+            <h1>Well Sampler</h1>
+            <h2>Track, Calculate, and Store Well Sampling Data</h2>
+            <button>Login</button>
+            <button>Sign Up</button>
+        </div>
+    )
+}
+
+export default MainPage
